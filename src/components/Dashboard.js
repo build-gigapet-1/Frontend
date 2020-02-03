830e1a8f6450f8e0ad60e5f0bfa1ce65431ac4ca
mlazzell17@gmail.com <MicheSi>
2020-02-03 11:25:23 -0700

    started to build dashboard and meal card

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,1 +1,21 @@
-//This component should 
+import React, {useState} from 'react';
+import MealCard from './MealCard';
+
+const Dashboard = props => {
+    const [meals, setMeals] = useState([]);
+
+    return (
+        <div>
+            <h2>Dashboard will go here</h2>
+            {meals.map(meal => (
+                <MealCard
+                 key={meal.id}
+                 
+                />
+            ))}
+        </div>
+        
+    )
+}
+
+export default Dashboard;
---
+++ src/components/MealCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MealCard = props => {
+    return (
+        <div className='mealCard'>
+            <h2>Date</h2>
+            <h3>Meal Type</h3>
+            <p>Fruits & Veggies: </p>
+            <p>Protein: </p>
+            <p>Grains: </p>
+            <p>Sweets: </p>
+        </div>
+    )
+}
+
+export default MealCard;
