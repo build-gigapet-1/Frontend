d7212ca0b5a97021900dcaf8cc1b7aa9acecd481
Your Name <you@example.com>
2020-02-05 17:28:52 -0500

    meallist

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -1,10 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import MealCard from './MealCard';
 import NavBarDashboard from './NavBarDashboard';
+import axios from 'axios';
+import { Container, Row } from "reactstrap";
+import styled from "styled-components";
+
 
 const MealList = props => {
     const [meals, setMeals] = useState([]);
 
+    useEffect(() =>{
+        axios
+            .get('https://gigapet-1.herokuapp.com/api/pets/')
+            .then(res => {
+                console.log(res);
+                setMeals(res.data.meals);
+            })
+            .catch(error => {
+                console.log('no meal found', error);
+            })
+    }, []);
+    
     // useEffect with axios call to get meal data
 
     return (
@@ -12,7 +28,24 @@ const MealList = props => {
             <NavBarDashboard />
             <h2>My Meals</h2>
             {/* map over meals to render meal cards */}
-            <MealCard />
+            <div className="mealList">
+            <NavBarDashboard />
+            <h2>My Meals</h2>
+            {/* map over meals to render meal cards */}
+            {/* <div>
+                {meals.map(meal => {
+                    <MealCard 
+                    key = {mealId}
+                    mealType = {meal.mealType}
+                    fruitsVeg = {meal.fruitsVeg}
+                    protein = {meal.protein}
+                    grains = {meal.grains}
+                    sweets = {meal.sweets}
+                    mealscore = {meal.mealscore}
+                    />
+                })}
+            </div> */}
+        </div>
         </div>
 
         
