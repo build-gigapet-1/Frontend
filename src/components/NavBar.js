d7088a9c247fd9113e5f13905d209c92097dacb0
mlazzell17@gmail.com <MicheSi>
2020-02-06 16:36:23 -0700

    fixed both navs for mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import React, {useState} from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import logo from '../images/logo.png';
 
 const NavBar = props => {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleNavbar = () => setCollapsed(!collapsed);
+
     return (
+        <div className='navContainer'>   
         <Nav className='dashNav'>
             <a href='https://kkslider2130.github.io/Gigapet-Marketing-proto/#'><img src={logo} alt='logo'/></a>
             <div className='links'>
@@ -20,16 +25,27 @@ const NavBar = props => {
                     <NavLink href="https://kkslider2130.github.io/Gigapet-Marketing-proto/team.html">Our Team</NavLink>
                 </NavItem>
             </div>
+            </Nav>
             <div className='customSelect'>
-                <select>
-                    <option value="https://kkslider2130.github.io/Gigapet-Marketing-proto/#" selected="selected">Menu</option> 
-                    <option value="/">Home</option> 
-                    <option value="/login">Log In</option> 
-                    <option value="/register">Register</option> 
-                    <option value="https://kkslider2130.github.io/Gigapet-Marketing-proto/team.html">Our Team</option> 
-                </select>
+                <Navbar className='mobileNav' color="faded" dark>
+                    <NavbarBrand href="/" className="mr-auto"><img src={logo} alt='logo'/></NavbarBrand>
+                    <NavbarToggler onClick={toggleNavbar} className="mr-2" color='dark'/>
+                    <Collapse isOpen={!collapsed} navbar>
+                        <Nav className='toggleNav' navbar>
+                        <NavItem>
+                            <NavLink href="/login">Log In</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/registration">Register</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://kkslider2130.github.io/Gigapet-Marketing-proto/team.html">Our Team</NavLink>
+                        </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
             </div>
-        </Nav>
+        </div>
     )
 }
 
