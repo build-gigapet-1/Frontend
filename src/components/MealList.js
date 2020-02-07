8e9c8557c4220bbbfc2d4831628a66303d03f61e
mlazzell17@gmail.com <MicheSi>
2020-02-06 19:07:23 -0700

    added filter meals dropdown

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -9,13 +9,13 @@ import axiosWithAuth from '../utils/AxiosWithAuth'
 const MealList = props => {
     const [meals, setMeals] = useState([]);
     const [pets,setPets] = useState([])
+    
     useEffect(() =>{
 
         axiosWithAuth()
                 .get('/pets/')
                 .then(res => {
                     setPets(res.data);
-
         })
         .catch(err => console.log('Cannot fetch pets', err))
       
@@ -23,52 +23,67 @@ const MealList = props => {
     }, []);
 
     const getMeals= e => {
-
-        console.log(e.target.value)
         axiosWithAuth()
             .get(`/pets/${e.target.value}`)
             .then(res => {
             console.log(res);
             setMeals(res.data.meals);
+            
         }).catch(error => {
             console.log('no meal found', error);
         })
-
-
     }
+
+    const filterMeals = e => {
+        const currentPet = props.petId;
+        console.log('current pet id', currentPet)
+        if (props.mealType === e.target.value) {
+
+        }
+    }
+
     
 
     return (
         <div className="mealList">
             <NavBarDashboard />
-            <h2>Which gigapets meals do you want to view?</h2>
-                <select name="petId" onChange={getMeals}>
-                    <option>Select Pet</option>
-                { pets.map(pet => (
-                    <option name="petId" value={pet.petId}> {pet.petName} </option>
-                ))}
-                </select>
-            <h2>My Meals</h2>
-            <div className="mealList">
-            <div>
-                {meals.map(meal => (
-                    <MealCard 
-                    mealId = {meal.mealId}
-                    date = {meal.date}
-                    mealType = {meal.mealType}
-                    fruitsVeg = {meal.fruitsVeg}
-                    protein = {meal.protein}
-                    grains = {meal.grains}
-                    sweets = {meal.sweets}
-                    mealScore = {meal.mealScore}
-                    petId = {meal.petId}
-                    />
-                )) }
+            <div className='mealsContainer'>
+                <div className='sortColumn'>
+                    <h3>Filter meals</h3>
+                    <select name='mealType' onChange={filterMeals}>
+                        <option>Meal Type</option>
+                        <option value='Breakfast'>Breakfast</option>
+                        <option value='Lunch'>Lunch</option>
+                        <option value='Dinner'>Dinner</option>
+                    </select>
+                </div>
+                <div className='mealsColumn'>
+                    <h2>Which gigapets meals do you want to view?</h2>
+                    <select name="petId" onChange={getMeals}>
+                        <option>Select Pet</option>
+                        { pets.map(pet => (
+                        <option name="petId" value={pet.petId}> {pet.petName} </option>
+                        ))}
+                    </select>
+                    <h2 className='myMeals'>My Meals</h2>
+                    <div className="mealCards">
+                        {meals.map(meal => (
+                            <MealCard 
+                            mealId = {meal.mealId}
+                            date = {meal.date}
+                            mealType = {meal.mealType}
+                            fruitsVeg = {meal.fruitsVeg}
+                            protein = {meal.protein}
+                            grains = {meal.grains}
+                            sweets = {meal.sweets}
+                            mealScore = {meal.mealScore}
+                            petId = {meal.petId}
+                            />
+                        )) }
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
-
-        
     )
 }
 
